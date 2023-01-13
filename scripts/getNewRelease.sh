840eb0942ce050300ebbcf8ea588334903891c4f
tdedobbeleer <tdedobbeleer@gmail.com>
2023-01-13 21:21:52 +0100

    add awk, to lose build info

--- scripts/getNewRelease.sh
+++ scripts/getNewRelease.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 VAR1=$RELEASE_NEW
-VAR2=$( curl --silent -L "https://hub.docker.com/v2/repositories/${DOCKER_USER}/soccer-ws/tags/?page_size=1000" | jq '.results | .[] | .name' -r | sed 's/latest//' | sort --version-sort | tail -n 1)
+VAR2=$( curl --silent -L "https://hub.docker.com/v2/repositories/${DOCKER_USER}/soccer-ws/tags/?page_size=1000" | jq '.results | .[] | .name' -r | sed 's/latest//' | sort --version-sort | tail -n 1 | awk -F '_' '{print $1}')
+
+echo "The latest release is ${RELEASE_NEW}"
+echo "The current docker release is ${RELEASE}"
 
 if [ "$VAR1" = "$VAR2" ]; then
     echo "No new latest tag, doing absolutely nothing."
 else
     export RELEASE=$VAR1
-    echo "${RELEASE}" > release
 fi
