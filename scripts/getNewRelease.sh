5be8b286f20537774a77c0a333126ead7c6344b3
tdedobbeleer <tdedobbeleer@gmail.com>
2023-01-13 21:25:35 +0100

    wrongly named variable

--- scripts/getNewRelease.sh
+++ scripts/getNewRelease.sh
@@ -3,8 +3,8 @@
 VAR1=$RELEASE_NEW
 VAR2=$( curl --silent -L "https://hub.docker.com/v2/repositories/${DOCKER_USER}/soccer-ws/tags/?page_size=1000" | jq '.results | .[] | .name' -r | sed 's/latest//' | sort --version-sort | tail -n 1 | awk -F '_' '{print $1}')
 
-echo "The latest release is ${RELEASE_NEW}"
-echo "The current docker release is ${RELEASE}"
+echo "The latest release is ${VAR1}"
+echo "The current docker release is ${VAR2}"
 
 if [ "$VAR1" = "$VAR2" ]; then
     echo "No new latest tag, doing absolutely nothing."
