cfebd60a5773c87770d06fba7c1b8d031727de2d
tdedobbeleer <tdedobbeleer@gmail.com>
2023-01-13 19:55:42 +0100

    do not output to files

--- scripts/getNewRelease.sh
+++ scripts/getNewRelease.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-VAR1=$(cat release.new | sed 's/ *$//g')
+VAR1=$RELEASE_NEW
 VAR2=$(cat release | sed 's/ *$//g')
 
 if [ "$VAR1" = "$VAR2" ]; then
     echo "No new latest tag, doing absolutely nothing."
 else
     export RELEASE=$VAR1
-    rm -rf release.new
     echo "${RELEASE}" > release
-    rm -rf release.new
 fi
