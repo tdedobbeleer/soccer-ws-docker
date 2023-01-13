80875f4e7a89c9970a5f85ce9d6f46139576c8c6
tdedobbeleer <tdedobbeleer@gmail.com>
2023-01-13 20:50:50 +0100

    lose the push

--- scripts/getNewRelease.sh
+++ scripts/getNewRelease.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 VAR1=$RELEASE_NEW
-VAR2=$(cat release | sed 's/ *$//g')
+VAR2=$( curl --silent -L "https://hub.docker.com/v2/repositories/${DOCKER_USER}/soccer-ws/tags/?page_size=1000" | jq '.results | .[] | .name' -r | sed 's/latest//' | sort --version-sort | tail -n 1)
 
 if [ "$VAR1" = "$VAR2" ]; then
     echo "No new latest tag, doing absolutely nothing."
