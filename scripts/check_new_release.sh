70ecfb0e67c96add0f1c88b4904f35cdb242c6cf
tdedobbeleer <tdedobbeleer@gmail.com>
2023-01-14 17:23:17 +0100

    some missing spaces

--- scripts/check_new_release.sh
+++ scripts/check_new_release.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-VAR1=$(curl -sL https://api.github.com/repos/tdedobbeleer/soccer-ws/tags |jq -r ".[].name" | head -n1)
-VAR2=$( curl --silent -L "https://hub.docker.com/v2/repositories/${DOCKER_USER}/soccer-ws/tags/?page_size=1000" | jq '.results | .[] | .name' -r | sed 's/latest//' | sort --version-sort | tail -n 1 | awk -F '_' '{print $1}')
+VAR1=$( curl -sL "https://api.github.com/repos/tdedobbeleer/soccer-ws/tags" | jq -r ".[].name" | head -n1 )
+VAR2=$( curl -sL "https://hub.docker.com/v2/repositories/${DOCKER_USER}/soccer-ws/tags/?page_size=1000" | jq '.results | .[] | .name' -r | sed 's/latest//' | sort --version-sort | tail -n 1 | awk -F '_' '{print $1}')
 
 echo "The latest release is ${VAR1}."
 echo "The current docker release is ${VAR2}."
