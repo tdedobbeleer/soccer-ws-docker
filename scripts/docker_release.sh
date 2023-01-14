d3ef2dcc98aabe6e84118c61a16d6294236decb3
tdedobbeleer <tdedobbeleer@gmail.com>
2023-01-14 17:04:12 +0100

    move condition to script, presumably the condition was checked before all the logic

--- scripts/docker_release.sh
+++ scripts/docker_release.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
-#Get the latest .jar
-curl https://github.com/tdedobbeleer/soccer-ws/releases/download/$RELEASE/ws-$RELEASE.jar --output soccer-ws.jar
+if [ -v ${RELEASE} ]; then
+  #Get the latest .jar
+  curl https://github.com/tdedobbeleer/soccer-ws/releases/download/$RELEASE/ws-$RELEASE.jar --output soccer-ws.jar
 
-#Login
-echo "${DOCKER_PASSWORD}" | docker login --username $DOCKER_USER --password-stdin
+  #Login
+  echo "${DOCKER_PASSWORD}" | docker login --username $DOCKER_USER --password-stdin
 
-#Build for all archs
-docker buildx create --use
-docker buildx build --build-arg RELEASE=${TRAVIS_TAG} --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag $DOCKER_USER/soccer-ws:latest .
-docker buildx build --build-arg RELEASE=${TRAVIS_TAG} --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag $DOCKER_USER/soccer-ws:${RELEASE}_${TRAVIS_BUILD_NUMBER} .
+  #Build for all archs
+  docker buildx create --use
+  docker buildx build --build-arg RELEASE=${TRAVIS_TAG} --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag $DOCKER_USER/soccer-ws:latest .
+  docker buildx build --build-arg RELEASE=${TRAVIS_TAG} --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag $DOCKER_USER/soccer-ws:${RELEASE}_${TRAVIS_BUILD_NUMBER} .
+else
+  echo "RELEASE variable not set, doing nothing."
+fi
