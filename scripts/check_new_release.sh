705630680ff6064c7f568c43fc451247af1dac46
tdedobbeleer <tdedobbeleer@gmail.com>
2023-01-14 17:49:34 +0100

    rewrite

--- scripts/check_new_release.sh
+++ scripts/check_new_release.sh
@@ -1,16 +1,11 @@
 #!/usr/bin/env bash
+echo "The latest tag is ${TAG}."
+echo "The current docker release is ${RELEASE}."
 
-VAR1=$( curl -sL "https://api.github.com/repos/tdedobbeleer/soccer-ws/tags" | jq -r ".[].name" | head -n1 )
-VAR2=$( curl -sL "https://hub.docker.com/v2/repositories/${DOCKER_USER}/soccer-ws/tags/?page_size=1000" | jq '.results | .[] | .name' -r | sed 's/latest//' | sort --version-sort | tail -n 1 | awk -F '_' '{print $1}')
-
-echo "The latest release is ${VAR1}."
-echo "The current docker release is ${VAR2}."
-
-if [ "$VAR1" = "$VAR2" ]; then
+if [ "$TAG" = "$RELEASE" ]; then
     echo "No new latest tag present."
     exit 1
 else
-    export RELEASE=$VAR1
-    echo "New release set to ${RELEASE}."
+    echo "New release set to ${TAG}."
     exit 0
 fi
--- scripts/docker_release.sh
+++ scripts/docker_release.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-echo "Pushing docker image version ${RELEASE}_${TRAVIS_BUILD_NUMBER} and tagging latest"
+echo "Pushing docker image version ${TAG}_${TRAVIS_BUILD_NUMBER} and tagging latest"
 #Get the latest .jar
-curl -sL https://github.com/tdedobbeleer/soccer-ws/releases/download/$RELEASE/ws-$RELEASE.jar --output soccer-ws.jar
+curl -sL https://github.com/tdedobbeleer/soccer-ws/releases/download/$TAG/ws-$TAG.jar --output soccer-ws.jar
 
 #Login
 echo "${DOCKER_PASSWORD}" | docker login --username $DOCKER_USER --password-stdin
@@ -10,4 +10,4 @@ echo "${DOCKER_PASSWORD}" | docker login --username $DOCKER_USER --password-stdi
 #Build for all archs
 docker buildx create --use
 docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag $DOCKER_USER/soccer-ws:latest .
-docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag $DOCKER_USER/soccer-ws:$RELEASE_$TRAVIS_BUILD_NUMBER .
+docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag $DOCKER_USER/soccer-ws:${TAG}_${TRAVIS_BUILD_NUMBER} .
