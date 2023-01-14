8a0849715cc48da8e17656099de3ef1784efd46d
tdedobbeleer <tdedobbeleer@gmail.com>
2023-01-14 16:35:48 +0100

    deploy -> point to script condition: regex

---
+++ scripts/docker_release.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+#Get the latest .jar
+curl https://github.com/tdedobbeleer/soccer-ws/releases/download/$RELEASE/ws-$RELEASE.jar --output soccer-ws.jar
+
+#Login
+echo "${DOCKER_PASSWORD}" | docker login --username $DOCKER_USER --password-stdin
+
+#Build for all archs
+docker buildx create --use
+docker buildx build --build-arg RELEASE=${TRAVIS_TAG} --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag $DOCKER_USER/soccer-ws:latest .
+docker buildx build --build-arg RELEASE=${TRAVIS_TAG} --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag $DOCKER_USER/soccer-ws:${RELEASE}_${TRAVIS_BUILD_NUMBER} .
