73c3ac30c62c97567dabdade6909a49c03b87fa7
tdedobbeleer <tdedobbeleer@gmail.com>
2023-01-15 20:05:12 +0100

    Add success check

--- scripts/docker_release.sh
+++ scripts/docker_release.sh
@@ -11,3 +11,14 @@ echo "${DOCKER_PASSWORD}" | docker login --username $DOCKER_USER --password-stdi
 docker buildx create --use
 docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag $DOCKER_USER/soccer-ws:latest .
 docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag $DOCKER_USER/soccer-ws:${TAG}_${TRAVIS_BUILD_NUMBER} .
+
+echo "Checking whether release was successfully pushed"
+RELEASE=$( curl -sL "https://hub.docker.com/v2/repositories/${DOCKER_USER}/soccer-ws/tags/?page_size=1000" | jq '.results | .[] | .name' -r | sed 's/latest//' | sort --version-sort | tail -n 1)
+
+if [ "${RELEASE}" = "${TAG}_${TRAVIS_BUILD_NUMBER}" ]; 
+then
+    exit 0;
+else
+    echo "Build failed, release not found on Docker hub.";
+    exit 1;
+fi
