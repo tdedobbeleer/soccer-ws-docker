a5d19a906369a855ef146f6b3b96ee0f78765f8a
tdedobbeleer <tdedobbeleer@gmail.com>
2023-01-16 13:58:54 +0100

    add tag name to image

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,10 @@
 FROM eclipse-temurin:latest
 
-COPY ./soccer-ws.jar /usr/app/soccer-ws.jar
+ARG JAR
+
+COPY ./$JAR /usr/app/$JAR
 
 RUN adduser --disabled-login --disabled-password --gecos "" javauser
 USER javauser
 
-ENTRYPOINT ["java", "-Xss512K","-XX:CICompilerCount=2","-Dfile.encoding=UTF-8","-XX:+UseContainerSupport","-Djava.security.egd=file:/dev/./urandom", "-jar","/usr/app/soccer-ws.jar", "--spring.profiles.active=${SPRING_PROFILES_ACTIVE:-local}"]
+ENTRYPOINT ["java", "-Xss512K","-XX:CICompilerCount=2","-Dfile.encoding=UTF-8","-XX:+UseContainerSupport","-Djava.security.egd=file:/dev/./urandom", "-jar","/usr/app/${JAR}", "--spring.profiles.active=${SPRING_PROFILES_ACTIVE:-local}"]
--- scripts/docker_release.sh
+++ scripts/docker_release.sh
@@ -2,15 +2,16 @@
 
 echo "Pushing docker image version ${TAG}_${TRAVIS_BUILD_NUMBER} and tagging latest"
 #Get the latest .jar
-curl -sL https://github.com/tdedobbeleer/soccer-ws/releases/download/$TAG/ws-$TAG.jar --output soccer-ws.jar
+JAR="soccer-ws-${TAG}.jar"
+curl -sL https://github.com/tdedobbeleer/soccer-ws/releases/download/$TAG/ws-$TAG.jar --output $JAR
 
 #Login
 echo "${DOCKER_PASSWORD}" | docker login --username $DOCKER_USER --password-stdin
 
 #Build for all archs
 docker buildx create --use
-docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag $DOCKER_USER/soccer-ws:latest .
-docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag $DOCKER_USER/soccer-ws:${TAG}_${TRAVIS_BUILD_NUMBER} .
+docker buildx build --build-arg JAR="${JAR}" --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag $DOCKER_USER/soccer-ws:latest .
+docker buildx build --build-arg JAR="${JAR}" --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag $DOCKER_USER/soccer-ws:${TAG}_${TRAVIS_BUILD_NUMBER} .
 
 echo "Checking whether release was successfully pushed"
 RELEASE=$( curl -sL "https://hub.docker.com/v2/repositories/${DOCKER_USER}/soccer-ws/tags/?page_size=1000" | jq '.results | .[] | .name' -r | sed 's/latest//' | sort --version-sort | tail -n 1)
