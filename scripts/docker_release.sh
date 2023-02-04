ab0a36b1734c3e764605bbaa24ae9244021c5c19
tdedobbeleer <tdedobbeleer@gmail.com>
2023-02-04 14:36:41 +0100

    Circleci project setup (#1)
    * Add .circleci/config.yml
    
    * test circleci
    
    * fix scripts, vars
    
    * right, move scripts dir, its in the working directory
    
    * add setup docker
    
    * earlier docker version + install jq
    
    * use sudo for jq
    
    * remove the TAG variable from jobs
    
    * allow buildx (experimental)
    
    * allow buildx (experimental)
    
    * add sleep
    
    * try to fix release check bug
    
    * just use curl to check whether build was succeeded
    
    * only run on master

--- scripts/docker_release.sh
+++ scripts/docker_release.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
+export DOCKER_CLI_EXPERIMENTAL=enabled
 
 build () {
   docker buildx build --build-arg JAR="${JAR}" --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag "${DOCKER_USER}/soccer-ws:${1}" .
 }
 
-echo "Pushing docker image version ${TAG}_${TRAVIS_BUILD_NUMBER} and tagging latest"
+TAG=$( curl -sL "https://api.github.com/repos/tdedobbeleer/soccer-ws/tags" | jq -r ".[].name" | head -n1 )
+
+echo "Pushing docker image version ${TAG}_${CIRCLE_BUILD_NUM} and tagging latest"
 #Get the latest .jar
 JAR="soccer-ws-${TAG}.jar"
 curl -sL https://github.com/tdedobbeleer/soccer-ws/releases/download/$TAG/ws-$TAG.jar --output $JAR
@@ -13,19 +16,12 @@ curl -sL https://github.com/tdedobbeleer/soccer-ws/releases/download/$TAG/ws-$TA
 echo "${DOCKER_PASSWORD}" | docker login --username $DOCKER_USER --password-stdin
 
 #Build for all archs
-docker buildx create --use
+docker context create buildx-build
+docker buildx create --use buildx-build
 
 build "latest"
 build "${TAG}"
-build "${TAG}_${TRAVIS_BUILD_NUMBER}"
+build "${TAG}_${CIRCLE_BUILD_NUM}"
 
 echo "Checking whether release was successfully pushed"
-RELEASE=$( curl -sL "https://hub.docker.com/v2/repositories/${DOCKER_USER}/soccer-ws/tags/?page_size=1000" | jq '.results | .[] | .name' -r | sed 's/latest//' | sort --version-sort | tail -n 1)
-
-if [ "${RELEASE}" = "${TAG}_${TRAVIS_BUILD_NUMBER}" ]; 
-then
-    exit 0;
-else
-    echo "Build failed, release not found on Docker hub.";
-    exit 1;
-fi
+curl --fail -sL "https://hub.docker.com/v2/repositories/${DOCKER_USER}/soccer-ws/tags/${TAG}_${CIRCLE_BUILD_NUM}"
